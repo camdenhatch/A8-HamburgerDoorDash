2e4e1b7741baa735c679c3d3b3550a8ee84535ff
camdenhatch <camden.hatch@gmail.com>
2024-03-11 13:41:59 -0600

    Commented out the basic structure

---
+++ a8.py
@@ -0,0 +1,17 @@
+# Camden Hatch, Rasa Ichimori, Machael Backman, Jooyoung Jeon, Porter Rasmussen
+# Section 4
+
+# These are all the classes:
+
+
+
+# For loop:
+
+    # Adding 100 customers into queue:
+
+    # Customer's name as key in dictionary:
+
+
+
+# Print list of customers and total burgers ordered sorted by most number of burgers:
+
