ceadd8daac4376eaafb621e1a646b0dcd369459a
rasa ichimori <rasaichimori@gmail.com>
2024-03-11 13:53:08 -0600

    add person class

--- a8.py
+++ a8.py
@@ -1,8 +1,19 @@
 # Camden Hatch, Rasa Ichimori, Machael Backman, Jooyoung Jeon, Porter Rasmussen
 # Section 4
 
+import random
+
 # These are all the classes:
 
+class Person:
+    def __init__(self):
+        self.customer_name = self.randomName()
+
+
+    def randomName(self):
+        lsCustomers = ["Jefe", "El Guapo", "Lucky Day", "Ned Nederlander", "Dusty Bottoms", "Harry Flugleman", "Carmen", "Invisible Swordsman", "Singing Bush"]
+        index = random.randint(0, len(lsCustomers) - 1)
+        return lsCustomers[index]
 
 
 # For loop:
